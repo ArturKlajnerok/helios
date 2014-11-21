83b7b6e562b14ce7d5f6f6b1556da37855ee3dc1
ArturKlajnerok <a.klajnerok@gmail.com>
2014-11-21 19:58:53 +0100

    [PLATFORM-549] Redis storage for OAuth2

---
+++ storage/redis_storage.go
@@ -0,0 +1,175 @@
+package storage
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/RangelReale/osin"
+	"github.com/garyburd/redigo/redis"
+	"time"
+)
+
+const (
+	CLIENT_PREFIX    = "client."
+	AUTHORIZE_PREFIX = "authorization."
+	ACCESS_PREFIX    = "access."
+)
+
+type RedisStorage struct {
+	pool   *redis.Pool
+	prefix string
+}
+
+func NewRedisStorage(server, prefix string) *RedisStorage {
+	pool := newPool(server, "")
+	return &RedisStorage{pool: pool, prefix: prefix}
+}
+
+func newPool(server, password string) *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+func (store *RedisStorage) Close() {}
+
+func (storage *RedisStorage) Clone() osin.Storage {
+	return storage
+}
+
+func (storage *RedisStorage) GetClient(id string) (osin.Client, error) {
+	key := CLIENT_PREFIX + id
+	clientJSON, err := storage.GetKey(key)
+	if err != nil {
+		return nil, err
+	}
+
+	client := new(osin.DefaultClient)
+	if err := json.Unmarshal([]byte(clientJSON), &client); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func (storage *RedisStorage) SetClient(id string, client osin.Client) error {
+	key := CLIENT_PREFIX + id
+	clientJSON, err := json.Marshal(client)
+	if err != nil {
+		return err
+	}
+
+	conn := storage.pool.Get()
+	defer conn.Close()
+	_, err = conn.Do("SET", key, clientJSON)
+	return err
+}
+
+func (storage *RedisStorage) SaveAuthorize(data *osin.AuthorizeData) error {
+	key := AUTHORIZE_PREFIX + data.Code
+	dataJSON, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	conn := storage.pool.Get()
+	defer conn.Close()
+	_, err = conn.Do("SET", key, dataJSON)
+	return err
+}
+
+func (storage *RedisStorage) LoadAuthorize(code string) (*osin.AuthorizeData, error) {
+	key := AUTHORIZE_PREFIX + code
+	authJSON, err := storage.GetKey(key)
+	if err != nil {
+		return nil, err
+	}
+
+	auth := new(osin.AuthorizeData)
+	if err := json.Unmarshal([]byte(authJSON), &auth); err != nil {
+		return nil, err
+	}
+
+	return auth, nil
+}
+
+func (storage *RedisStorage) RemoveAuthorize(code string) error {
+	key := AUTHORIZE_PREFIX + code
+	storage.DeleteKey(key)
+	return nil
+}
+
+func (storage *RedisStorage) SaveAccess(data *osin.AccessData) error {
+	key := ACCESS_PREFIX + data.AccessToken
+	dataJSON, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	conn := storage.pool.Get()
+	defer conn.Close()
+	_, err = conn.Do("SET", key, dataJSON)
+	return err
+}
+
+func (storage *RedisStorage) LoadAccess(token string) (*osin.AccessData, error) {
+	key := ACCESS_PREFIX + token
+	accessJSON, err := storage.GetKey(key)
+	if err != nil {
+		return nil, err
+	}
+
+	access := new(osin.AccessData)
+	if err := json.Unmarshal([]byte(accessJSON), &access); err != nil {
+		return nil, err
+	}
+
+	return access, nil
+}
+
+func (storage *RedisStorage) RemoveAccess(token string) error {
+	key := ACCESS_PREFIX + token
+	storage.DeleteKey(key)
+	return nil
+}
+
+func (storage *RedisStorage) LoadRefresh(token string) (*osin.AccessData, error) {
+	return nil, errors.New("Not implemented")
+}
+
+func (storage *RedisStorage) RemoveRefresh(token string) error {
+	return errors.New("Not implemented")
+}
+
+func (storage *RedisStorage) GetKey(keyName string) (string, error) {
+	db := storage.pool.Get()
+	defer db.Close()
+	value, err := redis.String(db.Do("GET", keyName))
+	if err != nil {
+		return "", err
+	}
+
+	return value, nil
+}
+
+func (storage *RedisStorage) DeleteKey(keyName string) bool {
+	db := storage.pool.Get()
+	defer db.Close()
+	_, err := db.Do("DEL", keyName)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
