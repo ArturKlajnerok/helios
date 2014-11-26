57efdfa9436803af68b62e83d6f6fcbeb4a754fb
ArturKlajnerok <a.klajnerok@gmail.com>
2014-11-26 15:49:22 +0100

    [PLATFORM-555] User model with mediawiki db support

---
+++ models/user.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"github.com/coopernurse/gorp"
+	"crypto/md5"
+	"encoding/hex"
+	"strconv"
+	"time"
+)
+
+desctype User struct {
+	Id                 int64     `db:"user_id"`
+	Name               string    `db:"user_name"`
+	RealName           string    `db:"user_real_name"`
+	Password           string    `db:"-"`
+	HashedPassword     string    `db:"user_password"`
+	NewPassword        string    `db:"user_newpassword"`
+	NewPassTime        string    `db:"user_newpass_time"`
+	Email              string    `db:"user_email"`
+	Touched            string    `db:"user_touched"`
+	Token              string    `db:"user_token"`
+	EmailAuthenticated string    `db:"user_email_authenticated"`
+	EmailToken         string    `db:"user_email_token"`
+	EmailTokenExpires  string    `db:"user_email_token_expires"`
+	Registration       string    `db:"user_registration"`
+	EditCount          int64     `db:"user_editcount"`
+	BirthDate          time.Time `db:"user_birthdate"`
+	Options            []byte    `db:"user_options"`
+}
+
+func (user *User) ValidPassword(password string) bool {
+	hasher := md5.New()
+	hasher.Write([]byte(password))
+	hash := hex.EncodeToString(hasher.Sum(nil))
+
+	hash = strconv.FormatInt(user.Id, 10) + "-" + hash
+
+	hasher.Reset()
+	hasher.Write([]byte(hash))
+	hash = hex.EncodeToString(hasher.Sum(nil))
+	return user.HashedPassword == hash
+}
+
+func (u *User) FindByName(dbmap *gorp.DbMap) bool {
+	err := dbmap.SelectOne(&u, "select * from user where user_name=?", u.Name)
+	if err != nil {
+		return false
+	}
+	return true
+}
