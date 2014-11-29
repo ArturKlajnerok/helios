310a7ccb3891977675c4b1b1d136a9e42b818254
ArturKlajnerok <a.klajnerok@gmail.com>
2014-11-29 19:08:50 +0100

    [PLATFORM-555] Fixed lint naming convetions in user test

--- models/user_test.go
+++ models/user_test.go
@@ -5,14 +5,14 @@ import (
 )
 
 const (
-	USER_ID            = 1
-	USER_PASSWORD      = "test"
-	USER_PASSWORD_HASH = "c4d81016667031737ffeda045105816e"
+	UserID           = 1
+	UserPassword     = "test"
+	UserPasswordHash = "c4d81016667031737ffeda045105816e"
 )
 
 func TestValidPassword(t *testing.T) {
-	user := User{Id: USER_ID, HashedPassword: USER_PASSWORD_HASH}
-	if !user.ValidPassword(USER_PASSWORD) {
-		t.Error("Expected hash: ", USER_PASSWORD_HASH)
+	user := User{Id: UserID, HashedPassword: UserPasswordHash}
+	if !user.ValidPassword(UserPassword) {
+		t.Error("Expected hash: ", UserPasswordHash)
 	}
 }
