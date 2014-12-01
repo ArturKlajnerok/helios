1258d3fedafe6f0935f348560626e71fd0bc1442
ArturKlajnerok <a.klajnerok@gmail.com>
2014-12-01 20:08:13 +0100

    [PLATFORM-555] Moved helios to separate struct + small refactor

---
+++ helios/helios.go
@@ -0,0 +1,81 @@
+package helios
+
+import (
+	"database/sql"
+	"errors"
+	"github.com/RangelReale/osin"
+	"github.com/Wikia/helios/config"
+	"github.com/Wikia/helios/models"
+	"github.com/Wikia/helios/storage"
+	"github.com/coopernurse/gorp"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Helios struct {
+	server *osin.Server
+	dbmap  *gorp.DbMap
+}
+
+func NewHelios() *Helios {
+	return new(Helios)
+}
+
+func (helios *Helios) tokenHandler(w http.ResponseWriter, r *http.Request) {
+	resp := helios.server.NewResponse()
+	defer resp.Close()
+	if ar := helios.server.HandleAccessRequest(resp, r); ar != nil {
+		switch ar.Type {
+		case osin.PASSWORD:
+			user := models.User{Name: ar.Username}
+			user.FindByName(helios.dbmap)
+			if user.IsValidPassword(ar.Password) {
+				ar.Authorized = true
+			}
+		}
+		helios.server.FinishAccessRequest(resp, r, ar)
+	}
+	if resp.IsError && resp.InternalError != nil {
+		log.Printf("ERROR: %s\n", resp.InternalError)
+	}
+	osin.OutputJSON(resp, w, r)
+}
+
+func (helios *Helios) initDb(dataSourceName string, dbConfig *config.DbConfig) {
+	db, err := sql.Open(dbConfig.Type, dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+	helios.dbmap = &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{dbConfig.Engine, dbConfig.Encoding}}
+	helios.dbmap.AddTableWithName(models.User{}, dbConfig.UserTable).SetKeys(true, dbConfig.UserTableKey)
+}
+
+func (helios *Helios) initServer(redisConfig *config.RedisConfig) {
+	osinConfig := osin.NewServerConfig()
+	osinConfig.AllowedAccessTypes = osin.AllowedAccessType{osin.PASSWORD, osin.REFRESH_TOKEN}
+	osinConfig.AllowGetAccessRequest = true
+	osinConfig.AllowClientSecretInParams = true
+
+	redisStorage := storage.NewRedisStorage(redisConfig)
+	helios.server = osin.NewServer(osinConfig, redisStorage)
+}
+
+func (helios *Helios) Run() {
+	if len(os.Args) < 2 {
+		log.Println("Provide mysql data source, like: user:pass@tcp(host:port)/dbname")
+		panic(errors.New("No data source provided"))
+	}
+	dataSourceName := os.Args[1]
+
+	conf := config.LoadConfig("./config/config.json")
+
+	helios.initDb(dataSourceName, conf.Db)
+	defer helios.dbmap.Db.Close()
+
+	helios.initServer(conf.Redis)
+
+	http.HandleFunc("/token", helios.tokenHandler)
+	http.ListenAndServe(conf.Server.Address, nil)
+}
--- main.go
+++ main.go
@@ -1,72 +1,9 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
-	"github.com/RangelReale/osin"
-	"github.com/Wikia/helios/config"
-	"github.com/Wikia/helios/models"
-	"github.com/Wikia/helios/storage"
-	"github.com/coopernurse/gorp"
-	_ "github.com/go-sql-driver/mysql"
-	"log"
-	"net/http"
-	"os"
+	"github.com/Wikia/helios/helios"
 )
 
-var server *osin.Server
-var dbmap *gorp.DbMap
-
-func tokenHandler(w http.ResponseWriter, r *http.Request) {
-	resp := server.NewResponse()
-	defer resp.Close()
-	if ar := server.HandleAccessRequest(resp, r); ar != nil {
-		switch ar.Type {
-		case osin.PASSWORD:
-			user := models.User{Name: ar.Username}
-			user.FindByName(dbmap)
-			if user.IsValidPassword(ar.Password) {
-				ar.Authorized = true
-			}
-		}
-		server.FinishAccessRequest(resp, r, ar)
-	}
-	if resp.IsError && resp.InternalError != nil {
-		log.Printf("ERROR: %s\n", resp.InternalError)
-	}
-	osin.OutputJSON(resp, w, r)
-}
-
-func initDb(dataSourceName string, dbConfig *config.DbConfig) *gorp.DbMap {
-	db, err := sql.Open(dbConfig.Type, dataSourceName)
-	if err != nil {
-		panic(err)
-	}
-	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{dbConfig.Engine, dbConfig.Encoding}}
-	dbmap.AddTableWithName(models.User{}, dbConfig.UserTable).SetKeys(true, dbConfig.UserTableKey)
-	return dbmap
-}
-
 func main() {
-	if len(os.Args) < 2 {
-		log.Println("Provide mysql data source, like: user:pass@tcp(host:port)/dbname")
-		panic(errors.New("No data source provided"))
-	}
-	dataSourceName := os.Args[1]
-
-	conf := config.LoadConfig("./config/config.json")
-
-	dbmap = initDb(dataSourceName, conf.Db)
-	defer dbmap.Db.Close()
-
-	osinConfig := osin.NewServerConfig()
-	osinConfig.AllowedAccessTypes = osin.AllowedAccessType{osin.PASSWORD, osin.REFRESH_TOKEN}
-	osinConfig.AllowGetAccessRequest = true
-	osinConfig.AllowClientSecretInParams = true
-
-	redisStorage := storage.NewRedisStorage(conf.Redis)
-	server = osin.NewServer(osinConfig, redisStorage)
-
-	http.HandleFunc("/token", tokenHandler)
-	http.ListenAndServe(conf.Server.Address, nil)
+	helios.NewHelios().Run()
 }
