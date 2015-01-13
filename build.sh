65fb98c61c55485c5d292db8ca81d360c871c900
jcellary <jaroslaw@wikia-inc.com>
2015-01-13 10:21:01 +0100

    Extended build script to support custom output

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-$GOPATH/bin/godep go build -o $GOPATH/bin/helios/helios github.com/Wikia/helios
+#!/bin/bash
+
+OUTPUT=$GOPATH/bin/helios/helios
+
+if [ $1 ]; then
+OUTPUT=$1
+fi;
+
+$GOPATH/bin/godep go build -o $OUTPUT github.com/Wikia/helios
